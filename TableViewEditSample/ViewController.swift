7956fdbb7d63a1ba7512e7a588f0f29221f78f26
Ryosuke Hiramatsu <himaratsu@gmail.com>
2015-09-05 01:36:57 +0900

    おまけを追加

--- TableViewEditSample/ViewController.swift
+++ TableViewEditSample/ViewController.swift
@@ -12,15 +12,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak private var tableView: UITableView!
     
+    var titles = ["0: Hello", "1: World", "2: Swift", "3: Programming"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setUpEditButton()
-    }
-    
-    private func setUpEditButton() {
         // (1) Edit ボタン
         navigationItem.leftBarButtonItem = editButtonItem()
+        
+//        tableView.allowsSelectionDuringEditing = true
     }
 
     // (2) Edit ボタン押された
@@ -32,20 +32,60 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // MARK: - UITableViewDataSource
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell")!
-        cell.textLabel?.text = "cell - \(indexPath.row)"
+        cell.textLabel?.text = titles[indexPath.row]
         return cell
     }
     
+    // (3) 削除を有効
     func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         return true
     }
     
+    // (4) 削除された時の処理
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // 先にデータを編集
+        titles.removeAtIndex(indexPath.row)
+        
+        // それからテーブルの更新
+        tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: indexPath.row, inSection: 0)],
+            withRowAnimation: UITableViewRowAnimation.Fade)
+    }
     
+    // (5) 移動を有効
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    // (6) 移動時の処理
+    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        
+        // データの順番を整える
+        let targetTitle = titles[sourceIndexPath.row]
+        if let index = titles.indexOf(targetTitle) {
+            titles.removeAtIndex(index)
+            titles.insert(targetTitle, atIndex: destinationIndexPath.row)
+            print(titles)
+        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        print(titles[indexPath.row])
+    }
 
+//    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+//        
+//        if tableView.editing {
+//            return UITableViewCellEditingStyle.Delete
+//        } else {
+//            return UITableViewCellEditingStyle.None
+//        }
+//    }
 }
 
